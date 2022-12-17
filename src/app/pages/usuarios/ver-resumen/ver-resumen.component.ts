0af4f3ca8ce5a428d6ce0b026af788bd50ec6413
Victor Abraham Bernedo Tapia <host29.3@gmail.com>
2022-12-16 23:40:15 -0500

    Cambios 1/5

--- src/app/pages/usuarios/ver-resumen/ver-resumen.component.ts
+++ src/app/pages/usuarios/ver-resumen/ver-resumen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CasopacienteService } from 'src/app/services/auroraapi/casopaciente.service';
 
 @Component({
   selector: 'app-ver-resumen',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerResumenComponent implements OnInit {
 
-  constructor() { }
+  g_routeparam_PacienteId: string = '0';
+
+  objAPIRpta_Full : any =
+  {
+    msnj : '',
+    rpta : {}
+  };
+
+  objAPIRpta_objPacienteFullInfo : any =
+  {
+    examenPreTestAutoestimaCompletado : false,
+    examenPreTestAutonomiaCompletado : false,
+    examenPreTestMotivacionAlCambioCompletado : false,
+    examenPreTestTomaDecisionCompletado : false,
+    examenPostTestAutoestimaCompletado : false,
+    examenPostTestAutonomiaCompletado : false,
+    examenPostTestMotivacionAlCambioCompletado : false,
+    examenPostTestTomaDecisionCompletado : false,
+    pacienteAnoDeEvaluacion : '',
+    pacienteApellidoMaterno : '',
+    pacienteApellidoPaterno : '',
+    pacienteCorreo : '',
+    pacienteDireccion : '',
+    pacienteDni : '',
+    pacienteFechaNacimiento : '',
+    pacienteId : '',
+    pacienteNombres : '',
+    pacienteRiesgo : '',
+    pacienteTipoViolencia : '',
+    pacienteTelefono : '',
+    psicologoId : '',
+  };
 
   ngOnInit(): void {
+    this.g_routeparam_PacienteId = this.route.snapshot.paramMap.get("pacienteid")??'0';
+    this.PintarLosDatosDelPacienteEnLaPantallaPrincipal(this.g_routeparam_PacienteId);
+  }
+
+  constructor(
+    private CasoPacienteService : CasopacienteService,
+    private route: ActivatedRoute
+  ) {
+
+  }
+
+  PintarLosDatosDelPacienteEnLaPantallaPrincipal(p_PacienteId : string)
+  {
+      this.CasoPacienteService.GetCasoPaciente_CasoPaciente_ByPacienteId(p_PacienteId)
+      .subscribe( APIRpta => {
+        console.log(APIRpta);
+        this.objAPIRpta_Full = APIRpta;
+        this.objAPIRpta_objPacienteFullInfo = this.objAPIRpta_Full.rpta;
+      })
   }
 
 }
--- src/app/services/auroraapi/casopaciente.service.ts
+++ src/app/services/auroraapi/casopaciente.service.ts
@@ -22,4 +22,11 @@ export class CasopacienteService {
 
   }
 
+  public GetCasoPaciente_CasoPaciente_ByPacienteId(PacienteId : string)
+  {
+      let headers = new HttpHeaders().set('Type-content','aplication/json')
+
+      return this.http.get(this._url + 'ObtenerCasoPacienteByPacienteId/' + PacienteId , { headers : headers});
+  }
+
 }
