95ce3d7336650610f6d790370f6fefd5f00f6657
saretaas123 <saraarrestegui123@hotmail.com>
2022-12-20 00:03:54 -0500

    Se modifico cuestionario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,15 +18,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent,
     NopageFoundComponent,
   ],
- 
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
     MatSlideToggleModule,
      BrowserAnimationsModule,
+     RouterModule
   ],
-  
+
   providers: [],
   bootstrap: [AppComponent]
 })
